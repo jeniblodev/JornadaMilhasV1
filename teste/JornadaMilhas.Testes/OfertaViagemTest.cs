b3d96ea22b49609edd681b6cb582e302f1ff782d
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2024-01-25 16:54:27 -0300

    video 2.1 - ensaio

--- teste/JornadaMilhas.Testes/OfertaViagemTest.cs
+++ teste/JornadaMilhas.Testes/OfertaViagemTest.cs
@@ -7,31 +7,48 @@ public class OfertaViagemTest
     [Fact]
     public void TestandoOfertaValida()
     {
-        //cenário - arrange
+        //arrange
         Rota rota = new Rota("OrigemTeste", "DestinoTeste");
         Periodo periodo = new Periodo(new DateTime(2024, 2, 25), new DateTime(2024, 2, 26));
         double preco = 100.0;
 
-        //ação - act
+        //act
         OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
 
-        //resultado - assert
+        //assert
         Assert.True(oferta.EhValido);
     }
 
     [Fact]
     public void TestandoOfertaComRotaNula()
     {
-        //cenário
+        //arrange
         Rota rota = null;
         Periodo periodo = new Periodo(new DateTime(2024, 2, 25), new DateTime(2024, 2, 26));
         double preco = 100.0;
 
-        //ação
+        //act
         OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
 
-        //resultado
+        //assert
         Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
         Assert.False(oferta.EhValido);
     }
+
+    [Fact]
+    public void TestandoPrecoNegativo()
+    {
+        //arrange
+        Rota rota = new Rota("Origem1", "Destino1");
+        Periodo periodo = new Periodo(new DateTime(2024, 8, 20), new DateTime(2024, 8, 30));
+        double preco = -250.0;
+
+        //act
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        //assert
+        Assert.Contains("O preço da oferta de viagem deve ser maior que zero.", oferta.Erros.Sumario);
+    }
+
+
 }
