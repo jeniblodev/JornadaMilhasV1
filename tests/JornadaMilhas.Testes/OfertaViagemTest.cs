987abf14d89c6d3a2f677ff580cd9ae1345f03b1
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2024-01-25 14:28:57 -0300

    video 1.4 - padrão AAA
    *Fizemos nosso primeiro teste de unidade
    * Vamos seguir testando o OfertaViagem e o próximo passo que podemos testar é a validação que está sendo  feita ao tentar criar um objeto com a rota ou período nulos - vamos focar primeiro na rota.
    
    *criar novo teste - TestandoOfertaComRotaNula
    *executar o teste
    
    *vamos parar pra analisar esses dois testes que a gente tem - quais os dados que temos aqui
    * temos:
    1 - uma parte onde a gente prepara o cenário do teste com os dados que vamos utilizar - ARRANGE
    2 - segunda parte onde executamos a ação que queremos validar naquele teste - ACT
    3 - última parte onde temos a verificação do resultado da ação - a validação do teste efetivamente - ASSERT
    
    * Essas três fases que os nossos testes estão seguindo tem um nome - padrão AAA ou triplo A
    * esse padrão é muito utilizado no universo de testes e ajuda a estruturar os testes de unidade deixando eles mais claros e compreensivos, facilitando a leitura, manutenção e compreensão dos testes - boa prática
    
    -> então aqui nós criamos mais um teste de unidade que permite a gente verificar se existe um erro quando a rota é nula e aprendemos também sobre o padrão AAA, que ajuda na escrita e padronização dos nossos testes.
    -> na próxima aula vamos continuar avançando no estudo de testes

--- tests/JornadaMilhas.Testes/OfertaViagemTest.cs
+++ tests/JornadaMilhas.Testes/OfertaViagemTest.cs
@@ -7,12 +7,31 @@ public class OfertaViagemTest
     [Fact]
     public void TestandoOfertaValida()
     {
+        //arrange
         Rota rota = new Rota("OrigemTeste", "DestinoTeste");
         Periodo periodo = new Periodo(new DateTime(2024, 1, 1), new DateTime(2024, 1, 5));
         double preco = 100.0;
 
+        //act
         OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
 
+        //assert
         Assert.True(oferta.EhValido);
+    }
+
+    [Fact]
+    public void TestandoOfertaComRotaNula()
+    {
+        //arrange
+        Rota rota = null;
+        Periodo periodo = new Periodo(new DateTime(2024, 1, 1), new DateTime(2024, 1, 5));
+        double preco = 100.0;
+
+        //act
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        //assert
+        Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+
     }
 }
