374ab6e0c9f9a06a18c1c8bb85bb07fa38b5f564
Jeniffer Bittencourt <lo.jbittencourt@gmail.com>
2024-01-25 15:29:36 -0300

    video 1.4-ensaio

--- teste/JornadaMilhas.Testes/OfertaViagemTest.cs
+++ teste/JornadaMilhas.Testes/OfertaViagemTest.cs
@@ -7,12 +7,31 @@ public class OfertaViagemTest
     [Fact]
     public void TestandoOfertaValida()
     {
+        //cenário - arrange
         Rota rota = new Rota("OrigemTeste", "DestinoTeste");
         Periodo periodo = new Periodo(new DateTime(2024, 2, 25), new DateTime(2024, 2, 26));
         double preco = 100.0;
 
+        //ação - act
         OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
 
+        //resultado - assert
         Assert.True(oferta.EhValido);
+    }
+
+    [Fact]
+    public void TestandoOfertaComRotaNula()
+    {
+        //cenário
+        Rota rota = null;
+        Periodo periodo = new Periodo(new DateTime(2024, 2, 25), new DateTime(2024, 2, 26));
+        double preco = 100.0;
+
+        //ação
+        OfertaViagem oferta = new OfertaViagem(rota, periodo, preco);
+
+        //resultado
+        Assert.Contains("A oferta de viagem não possui rota ou período válidos.", oferta.Erros.Sumario);
+        Assert.False(oferta.EhValido);
     }
 }
